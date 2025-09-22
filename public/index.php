7f6e13bb59afd8a5db6e11e163df2d94041c40f4
quynhhodev <quynh.dev2k1@gmail.com>
2025-09-22 10:07:05 +0700

    recovery index.php

--- public/index.php
+++ public/index.php
@@ -4,4 +4,6 @@ use App\Kernel;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
-echo 'a';
+return function (array $context) {
+    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
+};
