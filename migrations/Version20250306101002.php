72394ae9789f64b7e89a0c8f6fc82c43b2b7e912
quynhhodev <quynh.dev2k1@gmail.com>
2025-09-23 11:13:35 +0700

    make broken

--- migrations/Version20250306101002.php
+++ migrations/Version20250306101002.php
@@ -21,7 +21,7 @@ final class Version20250306101002 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE "user" (id SERIAL NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, PRIMARY KEY(id))');
-        $this->addSql('CREATE UNIQUE INDEX UNIQ_IDENTIFIER_EMAIL ON "user" (email)');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_IDENTIFIER_EMAIL ON "user" (emaill)');
     }
 
     public function down(Schema $schema): void
